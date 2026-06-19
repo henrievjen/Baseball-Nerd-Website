6ba3973559eb1179ab3fd29a1d0bd4eaab260a88
henrievjen <henrievjen@gmail.com>
2026-06-18 21:14:30 -0500

    Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'scores', pathMatch: 'full' },
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
   {
     path: 'scores',
     loadChildren: () => import('./scores/scores.module').then(m => m.ScoresModule)
